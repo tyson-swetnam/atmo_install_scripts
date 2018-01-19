f8853479f0fb889a8071ee0aa04955a6026feeac
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2018-01-19 08:57:05 -0700

    Create ezd.sh

---
+++ ezd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export REAL_USER=tyson_swetnam
+rm -rf /opt/cyverse-ez-docker
+apt-get install -y ansible
+ansible-pull -v -U https://github.com/cyverse/cyverse-ez-docker.git -d /opt/cyverse-ez-docker -i localhost -e ansible_docker_user=${REAL_USER} -f >> /var/log/cyverse-ez.log 2>&1
+
+ezd
